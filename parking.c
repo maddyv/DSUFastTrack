773c9dd2309f53ba34ba5914410e3d61168a463d
maddyv <42581851+maddyv@users.noreply.github.com>
2018-08-31 22:08:24 +0530

    Program with Documentation

--- parking.c
+++ parking.c
@@ -1,64 +1,68 @@
 #include <stdio.h>
 #include <math.h>
-void input(float* time, char* ty);
-int get_fare(float ti, char tp);
-void output(float to, char po);
-int main()
+void input(float* time, char* ty); // Input function prototype
+int get_fare(float ti, char tp); // Get fare function prototype
+void output(float to, char po); // Output function prototype
+int main() // Main function
 {
-    float t;
+    float t; // Init vars
     char ch;
-    input(&t,&ch);
-    output(t,ch);
-    return 0;
+    input(&t,&ch); // Pass addresses to input function
+    output(t,ch); // Call output function
+    return 0; // Terminate program
 }
 void input(float* time, char* ty)
 {
+    printf("Enter type of vehicle:\n"); // Request for input
+    scanf("%c", ty);
     printf("Enter time parked (in hours):\n ");
     scanf("%f", time);
-    printf("Enter type of vehicle: \n");
-    scanf("%c", ty);
+
 }
-int get_fare(float ti, char tp)
+int get_fare(float ti, char tp) // Compute function definition
 {
-    int rt;
+    int rt; // Init rate var
     int t1=(int) ceil(ti); // Rounding off to reflect actual parking fare systems
     switch(tp)
     {
-        case 'c':
+        case 'c': // Car case
             if(t1<=2)
             {
-                rt=20*t1;
+                rt=20*t1; // Inital fare
             }
             else
             {
-                rt=40+(30*(t1-2));
+                rt=40+(30*(t1-2)); // Inital fare + next fare
             }
+            break;
         case 'b':
             if(t1<=4)
             {
-                rt=40*t1;
+                rt=40*t1; // Inital fare
             }
             else
             {
-                rt=160+(50*(t1-4));
+                rt=160+(50*(t1-4)); // Inital fare + next fare
             }
+            break;
         case 't':
             if(t1<=3)
             {
-                rt=30*t1;
+                rt=30*t1; // Inital fare
             }
             else
             {
-                rt=90+(40*(t1-3));
+                rt=90+(40*(t1-3)); // Inital fare + next fare
             }
+            break;
         default:
-            return -1;
+            return -1; // Return number to print message to signify invalid input
     }
 }
-void output(float to, char po)
+void output(float to, char po) // output function definition
 {
-    int r=get_fare(to,po);
-    if(r!=-1)
+    int r=get_fare(to,po); // Rate var
+    if(r!=-1) // If valid input
     {
         printf("The fare to be paid is: %d",r);
     }
