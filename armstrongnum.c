2ca013f4508fa635d5256c35c2b3ec65fd112458
maddyv <42581851+maddyv@users.noreply.github.com>
2018-09-07 12:04:45 +0530

    Fixed Defintion of Armstrong Number

--- armstrongnum.c
+++ armstrongnum.c
@@ -1,5 +1,5 @@
 #include <stdio.h> // Include Standard I/O Library
-#include <math.h> // Include Math Library for ease of calling the power function
+#include <math.h> // Include Math Library to call the power function
 void input(int* ai); // Input function prototype
 void check_armstrong(int num); // Condition function prototype
 int main() // Main function
@@ -16,15 +16,22 @@ void input(int* ai) // Input function definition
 }
 void check_armstrong(int num) // Condition function definition
 {
+	int dc=0; // Init digit counter var
     int ncl=num; // Create clone of num
+	while(num>0)
+	{
+		dc++; // Counts digits
+		num=num/10; 
+	}
+	num=ncl; // Give num the original value
     int sum=0; // Var for sum of the digits
     int cn=0; // Var to temporarily store digits
     while(num>0) // Terminates when moved from one's place
     {
         cn=num%10; // Extract digit
-        sum=sum+ pow(cn,3); // Cube the digit and add
+        sum=sum+ pow(cn,dc); // Raise the digit to the appropriate power and add
         num=num/10; // Move to next place
-    }
+    }	
     if(sum==ncl) // If the sum of the cubes of the digits is equal to the number
     {
         printf("The number is an Armstrong number. \n");
