ef57d559f543e34d0f9aba5256e613fad182ad6b
maddyv <42581851+maddyv@users.noreply.github.com>
2018-09-14 11:10:26 +0530

    Used Structures for complex number

--- Assignment1.c
+++ Assignment1.c
@@ -3,6 +3,11 @@
 void read(int* ai, int* bi, int* ci); // Input function prototype
 double compute(int* a1, int* b1, int* c1); // Compute function prototype
 void output(int* a2, int* b2, int* c2, double* dcr1); // Output function prototype
+typedef struct complex
+{
+	double real;
+	double img;
+} comp;
 /*
 * main function receives no parameters:
 * Input parameters : none
@@ -14,7 +19,7 @@ int main() // Main function
 	int a, b, c; // Declaring variables to store co-efficients
 	read(&a,&b,&c); // Passing addresses of variables to read function
 	double dcr2=compute(&a,&b,&c); // Passing addresses of vars to compute function to obtain the discriminant value
-    output(&a,&b,&c,&dcr2); // Passing addresses of vars to output function
+        output(&a,&b,&c,&dcr2); // Passing addresses of vars to output function
 	return 0; // Program terminates
 }
 /*
@@ -50,25 +55,25 @@ double compute(int* a1, int* b1, int* c1) // Accepts pointers to the main variab
 * Return parameter : none
 */
 void output(int* a2, int* b2, int* c2, double* dcr1) // Outputs the required roots
-{
+{    
     double rt=0.0; // Declaring root var
-	double temp=0.0; // Declaring temp var to store complex part of root
     if(*dcr1==0.0) // If one root/two equal  roots are present
-	{
-		rt=(double)-((*b2)/(2.0*(*a2))); // Standard Formula
-		printf("There is one root and it is %.2lf\n",rt);
-	}
-	else if(*dcr1>0.0) // If two real unequal roots are present
-	{
-		rt=(double) (-*b2+sqrt((double)*dcr1))/(2.0*(*a2)); // Standard Formula
-		printf("There are two real roots i.e. %.2lf and ",rt);
-		rt=(double) (-*b2-sqrt((double)*dcr1))/(2.0*(*a2)); // Standard Formula
-		printf("%.2lf\n",rt);
-	}
-	else if(*dcr1<0.0) // If two complex unequal roots are present
     {
-        rt=(double)-(*b2/(2.0*(*a2))); // Standard Formula (real part)
-        temp=(double) (sqrt(-*dcr1))/(2*(*a2)); // Standard Formula (complex part)
-        printf("There are two complex roots i.e. %.2f + %.2fi and %.2f - %.2fi",rt,temp,rt,temp);
+	rt=(double)-((*b2)/(2.0*(*a2))); // Standard Formula
+	printf("There is one root and it is %.2lf\n",rt);
+    }
+    else if(*dcr1>0.0) // If two real unequal roots are present
+    {
+	rt=(double) (-*b2+sqrt((double)*dcr1))/(2.0*(*a2)); // Standard Formula
+	printf("There are two real roots i.e. %.2lf and ",rt);
+	rt=(double) (-*b2-sqrt((double)*dcr1))/(2.0*(*a2)); // Standard Formula
+	printf("%.2lf\n",rt);
+    }
+    else if(*dcr1<0.0) // If two complex unequal roots are present
+    {
+    	comp i1;
+        i1.real=(double)-(*b2/(2.0*(*a2))); // Standard Formula (real part)
+        i1.img=(double) (sqrt(-*dcr1))/(2*(*a2)); // Standard Formula (complex part)
+        printf("There are two complex roots i.e. %.2f + %.2fi and %.2f - %.2fi",i1.real,i1.img,i1.real,i1.img);
     }
 }
