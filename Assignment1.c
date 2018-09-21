fd0dcaf61eac8387f0bd88237dce0b842689ca79
maddyv <42581851+maddyv@users.noreply.github.com>
2018-09-22 00:01:11 +0530

    Fixed Compute function format

--- Assignment1.c
+++ Assignment1.c
@@ -1,13 +1,13 @@
 #include <stdio.h> // Importing Standard IO Package
 #include <math.h> // Importing Math package for the square root function
-void read(int* ai, int* bi, int* ci); // Input function prototype
-double compute(int* a1, int* b1, int* c1); // Compute function prototype
-void output(int* a2, int* b2, int* c2, double* dcr1); // Output function prototype
 typedef struct complex
 {
 	double real;
 	double img;
 } comp;
+void read(double* ai, double* bi, double* ci); // Input function prototype
+void compute(double a1, double b1, double c1,double* rt1, double* rt2, comp *i1, int* c); // Compute function prototype
+void output(double rto1, double rto2, comp io1, int c1); // Output function prototype
 /*
 * main function receives no parameters:
 * Input parameters : none
@@ -16,64 +16,79 @@ typedef struct complex
 */
 int main() // Main function
 {
-	int a, b, c; // Declaring variables to store co-efficients
+	double a, b, c; // Declaring variables to store co-efficients
 	read(&a,&b,&c); // Passing addresses of variables to read function
-	double dcr2=compute(&a,&b,&c); // Passing addresses of vars to compute function to obtain the discriminant value
-        output(&a,&b,&c,&dcr2); // Passing addresses of vars to output function
+	double rf1,rf2; // Init root vars
+	comp if1;
+	int cf1;
+	compute(a,b,c,&rf1,&rf2,&if1,&cf1);
+    output(rf1,rf2,if1,cf1); // Passing addresses of vars to output function
 	return 0; // Program terminates
 }
 /*
 * read function receives three parameters:
-* Input parameters int*: ai, bi, ci
+* Input parameters double*: ai, bi, ci
 * Output parameters : none
 * Return parameter : none
 */
-void read(int* ai, int* bi, int* ci) // Accepts pointers to the main variables
+void read(double* ai, double* bi, double* ci) // Accepts pointers to the main variables
 {
 	printf("Enter co-efficient \'a\' \n"); // Requests input of a
-	scanf("%d",ai);
+	scanf("%lf",ai);
 	printf("Enter co-efficient \'b\' \n"); // Requests input of b
-	scanf("%d",bi);
+	scanf("%lf",bi);
 	printf("Enter co-efficient \'c\' \n"); // Requests input of c
-	scanf("%d",ci);
+	scanf("%lf",ci);
 }
 /*
 * compute function receives three parameters:
-* Input parameters int*: a1, b1, c1
+* Input parameters double*: a1, b1, c1
 * Output parameters : none
 * Return parameter : The value of the discriminant
 */
-double compute(int* a1, int* b1, int* c1) // Accepts pointers to the main variables
+void compute(double a1, double b1, double c1,double* rt1, double* rt2, comp *i1, int* c) // Accepts pointers to the main variables
 {
-	double dcr=(double) ((*b1)*(*b1))-(4.0*(*a1)*(*c1)); // Calculates discriminant
-	return dcr; // Returns discriminant
+	double dcr=((b1)*(b1))-(4.0*(a1)*(c1)); // Calculates discriminant
+    if(dcr==0.0) // If one root/two equal roots are present
+    {
+        *rt1=-((b1)/(2.0*(a1))); // Standard Formula
+        *rt2=0;
+        *c=0;
+    }
+    else if(dcr>0.0) // If two real unequal roots are present
+    {
+        *rt1=(-b1+sqrt((double)dcr))/(2.0*(a1)); // Standard Formula
+        *rt2=(-b1-sqrt((double)dcr))/(2.0*(a1)); // Standard Formula
+        *c=1;
+    }
+    else if(dcr<0.0) // If two complex unequal roots are present
+    {
+        *c=2;
+        *rt1=0;
+        *rt2=0;
+        i1->real=-(b1/(2.0*(a1))); // Standard Formula (real part)
+        i1->img=(sqrt(-dcr))/(2*(a1)); // Standard Formula (complex part)
+    }
 }
 /*
 * output function receives one parameter:
-* Input parameters :int a2,b2,c2 and double dcr1
+* Input parameters :double a2,b2,c2 and double dcr1
 * Output parameters Prints: Roots of the Quadratic Equation
 * Return parameter : none
 */
-void output(int* a2, int* b2, int* c2, double* dcr1) // Outputs the required roots
-{    
-    double rt=0.0; // Declaring root var
-    if(*dcr1==0.0) // If one root/two equal  roots are present
+void output(double rto1, double rto2, comp io1, int c1) // Outputs the required roots
+{
+    if(c1==0) // If one root/two equal  roots are present
     {
-	rt=(double)-((*b2)/(2.0*(*a2))); // Standard Formula
-	printf("There is one root and it is %.2lf\n",rt);
+        printf("There is one root and it is %.2lf\n",rto1);
     }
-    else if(*dcr1>0.0) // If two real unequal roots are present
+    else if(c1==1) // If two real unequal roots are present
     {
-	rt=(double) (-*b2+sqrt((double)*dcr1))/(2.0*(*a2)); // Standard Formula
-	printf("There are two real roots i.e. %.2lf and ",rt);
-	rt=(double) (-*b2-sqrt((double)*dcr1))/(2.0*(*a2)); // Standard Formula
-	printf("%.2lf\n",rt);
+        printf("There are two real roots i.e. %.2lf and ",rto1);
+        printf("%.2lf\n",rto2);
     }
-    else if(*dcr1<0.0) // If two complex unequal roots are present
+    else if(c1==2) // If two complex unequal roots are present
     {
-    	comp i1;
-        i1.real=(double)-(*b2/(2.0*(*a2))); // Standard Formula (real part)
-        i1.img=(double) (sqrt(-*dcr1))/(2*(*a2)); // Standard Formula (complex part)
-        printf("There are two complex roots i.e. %.2f + %.2fi and %.2f - %.2fi",i1.real,i1.img,i1.real,i1.img);
+        printf("There are two complex roots i.e. %.2f + %.2fi and %.2f - %.2fi",io1.real,io1.img,io1.real,io1.img);
     }
 }
