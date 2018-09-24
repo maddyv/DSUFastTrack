203def491778d717c5393278bf8d6cc4b85f1337
maddyv <42581851+maddyv@users.noreply.github.com>
2018-09-24 17:33:14 +0530

    Removed unnecessary variable

--- Assignment1.c
+++ Assignment1.c
@@ -6,8 +6,8 @@ struct complex
 	double img;
 };
 void read(double* ai, double* bi, double* ci); // Input function prototype
-void compute(double a1, double b1, double c1,double* rt1, double* rt2, struct complex *i1, int* c); // Compute function prototype
-void output(double rto1, double rto2, struct complex io1, int c1); // Output function prototype
+void compute(double a1, double b1, double c1,double* rt1, double* rt2, struct complex *i1, double* dcr); // Compute function prototype
+void output(double rto1, double rto2, struct complex io1, double dcro1); // Output function prototype
 /*
 * main function receives no parameters:
 * Input parameters : none
@@ -18,11 +18,10 @@ int main() // Main function
 {
 	double a, b, c; // Declaring variables to store co-efficients
 	read(&a,&b,&c); // Passing addresses of variables to read function
-	double rf1,rf2; // Init root vars
+	double rf1,rf2,dcrm; // Init root vars
 	struct complex if1;
-	int cf1;
-	compute(a,b,c,&rf1,&rf2,&if1,&cf1);
-    output(rf1,rf2,if1,cf1); // Passing addresses of vars to output function
+	compute(a,b,c,&rf1,&rf2,&if1,&dcrm);
+    output(rf1,rf2,if1,dcrm); // Passing addresses of vars to output function
 	return 0; // Program terminates
 }
 /*
@@ -46,28 +45,25 @@ void read(double* ai, double* bi, double* ci) // Accepts pointers to the main va
 * Output parameters : none
 * Return parameter : The value of the discriminant
 */
-void compute(double a1, double b1, double c1,double* rt1, double* rt2, struct complex *i1, int* c) // Accepts pointers to the main variables
+void compute(double a1, double b1, double c1,double* rt1, double* rt2, struct complex *i1, double* dcr) // Accepts pointers to the main variables
 {
-	double dcr=((b1)*(b1))-(4.0*(a1)*(c1)); // Calculates discriminant
-    if(dcr==0.0) // If one root/two equal roots are present
+	*dcr=((b1)*(b1))-(4.0*(a1)*(c1)); // Calculates discriminant
+    if(*dcr==0.0) // If one root/two equal roots are present
     {
         *rt1=-((b1)/(2.0*(a1))); // Standard Formula
         *rt2=0;
-        *c=0;
     }
-    else if(dcr>0.0) // If two real unequal roots are present
+    else if(*dcr>0.0) // If two real unequal roots are present
     {
-        *rt1=(-b1+sqrt((double)dcr))/(2.0*(a1)); // Standard Formula
-        *rt2=(-b1-sqrt((double)dcr))/(2.0*(a1)); // Standard Formula
-        *c=1;
+        *rt1=(-b1+sqrt((double)*dcr))/(2.0*(a1)); // Standard Formula
+        *rt2=(-b1-sqrt((double)*dcr))/(2.0*(a1)); // Standard Formula
     }
-    else if(dcr<0.0) // If two complex unequal roots are present
+    else if(*dcr<0.0) // If two complex unequal roots are present
     {
-        *c=2;
         *rt1=0;
         *rt2=0;
         i1->real=-(b1/(2.0*(a1))); // Standard Formula (real part)
-        i1->img=(sqrt(-dcr))/(2*(a1)); // Standard Formula (complex part)
+        i1->img=(sqrt(-*dcr))/(2*(a1)); // Standard Formula (complex part)
     }
 }
 /*
@@ -76,18 +72,18 @@ void compute(double a1, double b1, double c1,double* rt1, double* rt2, struct co
 * Output parameters Prints: Roots of the Quadratic Equation
 * Return parameter : none
 */
-void output(double rto1, double rto2, struct complex io1, int c1) // Outputs the required roots
+void output(double rto1, double rto2, struct complex io1, double dcro1) // Outputs the required roots
 {
-    if(c1==0) // If one root/two equal  roots are present
+    if(dcro1==0.0) // If one root/two equal  roots are present
     {
         printf("There is one root and it is %.2lf\n",rto1);
     }
-    else if(c1==1) // If two real unequal roots are present
+    else if(dcro1>0.0) // If two real unequal roots are present
     {
         printf("There are two real roots i.e. %.2lf and ",rto1);
         printf("%.2lf\n",rto2);
     }
-    else if(c1==2) // If two complex unequal roots are present
+    else if(dcro1<0.0) // If two complex unequal roots are present
     {
         printf("There are two complex roots i.e. %.2f + %.2fi and %.2f - %.2fi",io1.real,io1.img,io1.real,io1.img);
     }
